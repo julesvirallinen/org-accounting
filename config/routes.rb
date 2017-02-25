069301d1f0964b512c5a13ac0861f3f9113fae0e
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-02-25 17:17:06 +0200

    Made new expense page default

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
 
 
-  root 'expenses#index'
+  root 'expenses#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
