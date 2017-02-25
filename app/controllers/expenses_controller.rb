487160b3bb09493e5dd2b6d2ffbcbeaad93d0687
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-02-25 17:28:04 +0200

    Sorts expenses by date

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -15,7 +15,7 @@ class ExpensesController < ApplicationController
     elsif params[:accepted] == "false"
       @expenses = Expense.where(accepted: false)
     else
-      @expenses = Expense.all
+      @expenses = Expense.all.order(:date)
     end
   end
 
