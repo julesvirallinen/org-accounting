c94c1884e3a766efab134b1b6fcfae2770bb3daa
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-02-27 00:09:25 +0200

    Validates receipt upload, remembers euros in form

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -5,8 +5,10 @@ class Expense < ActiveRecord::Base
   belongs_to :accounting_year
 
   validates :description, presence: true
-  validates :receipt_url, presence: true
+  validates :receipt_url, presence: true, format: { with: /(amazonaws)/, message: 'The receipt was not uploaded correctly, try again'}
   validates :euros, presence: true, numericality: {greater_than: 0}
+
+
 
   scope :accepted, -> { where accepted:true}
 
